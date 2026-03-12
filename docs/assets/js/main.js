06ceef549a185100e2d140595bf36d7da839f747
venkatreddyG1822 <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>
2026-03-12 13:03:17 +0530

    Fix internal links for GitHub Pages

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -421,3 +421,22 @@ this.innerHTML = '<i class="fa-solid fa-eye"></i>';
 });
 
 });
+const launchDate = new Date("Dec 30, 2026 00:00:00").getTime();
+
+const timer = setInterval(function(){
+
+const now = new Date().getTime();
+
+const distance = launchDate - now;
+
+const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+document.getElementById("days").innerText = days;
+document.getElementById("hours").innerText = hours;
+document.getElementById("minutes").innerText = minutes;
+document.getElementById("seconds").innerText = seconds;
+
+},1000);
